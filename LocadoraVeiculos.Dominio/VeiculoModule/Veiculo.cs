5489267a9464255fb0a10d24bf336ea01710a8cb
johnboeira <jbiel72001@gmail.com>
2021-08-12 17:10:16 -0300

    correção de problema em arquivos

--- LocadoraVeiculos.Dominio/VeiculoModule/Veiculo.cs
+++ LocadoraVeiculos.Dominio/VeiculoModule/Veiculo.cs
@@ -22,11 +22,64 @@ namespace LocadoraVeiculos.Dominio.VeiculoModule
         //foto
         int kilometragem;
         int id_grupoVeiculo;
-        
+
+        public Veiculo(string placa, string cor, string marca, int ano, string tipoCombustivel, double quantidadeLitros, int numeroPortas, int capacidadeDePessoas, string tamanhoPortaMalas, int kilometragem, int id_grupoVeiculo)
+        {
+            this.placa = placa;
+            this.cor = cor;
+            this.marca = marca;
+            this.ano = ano;
+            this.tipoCombustivel = tipoCombustivel;
+            this.quantidadeLitros = quantidadeLitros;
+            this.numeroPortas = numeroPortas;
+            this.capacidadeDePessoas = capacidadeDePessoas;
+            this.tamanhoPortaMalas = tamanhoPortaMalas;
+            this.kilometragem = kilometragem;
+            this.id_grupoVeiculo = id_grupoVeiculo;
+        }
 
         public override string Validar()
         {
-
+            if (string.IsNullOrEmpty(placa))
+            {
+                return "Campo placa é obrigatório";
+            }
+            else if (string.IsNullOrEmpty(cor))
+            {
+                return "Campo cor é obrigatório";
+            }
+            else if (string.IsNullOrEmpty(marca))
+            {
+                return "Campo marca é obrigatório";
+            }
+            else if (ano == 0)
+            {
+                return "Campo ano é obrigatório";
+            }
+            else if (string.IsNullOrEmpty(tipoCombustivel))
+            {
+                return "Campo tipo combustível é obrigatório";
+            }
+            else if (quantidadeLitros == 0)
+            {
+                return "Campo quantiade de litros do tanque é obrigatório";
+            }
+            else if (numeroPortas == 0)
+            {
+                return "Campo Número de Portas é obrigatório";
+            }
+            else if (capacidadeDePessoas == 0)
+            {
+                return "Campo capacidade de pessoas é obrigatório";
+            }
+            else if (string.IsNullOrEmpty(tamanhoPortaMalas))
+            {
+                return "Campo tamanho do porta-malas é obrigatório";
+            }
+            else if (id_grupoVeiculo == 0)
+            {
+                return "Campo grupo de veículo é obrigatório";
+            }
 
             return "VALIDO";
         }
