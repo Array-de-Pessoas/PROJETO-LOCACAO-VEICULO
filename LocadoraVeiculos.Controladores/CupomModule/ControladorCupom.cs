18087316be0fbc95d9fecf6184ee11b8a6f255a5
SirLucasWolff <lucaswolff2014@hotmail.com>
2021-09-02 15:26:38 -0300

    Primeiras etapas do cadastro de cupom.

---
+++ LocadoraVeiculos.Controladores/CupomModule/ControladorCupom.cs
@@ -0,0 +1,43 @@
+﻿using LocadoraVeiculos.Controladores.Shared;
+using LocadoraVeiculos.Dominio.CupomModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraVeiculos.Controladores.CupomModule
+{
+    public class ControladorCupom : Controlador<Cupom>
+    {
+        public override string Editar(int id, Cupom registro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Existe(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string InserirNovo(Cupom registro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Cupom SelecionarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Cupom> SelecionarTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LocadoraVeiculos.Dominio/CupomModule/Cupom.cs
@@ -0,0 +1,22 @@
+﻿using LocadoraVeiculos.Dominio.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraVeiculos.Dominio.CupomModule
+{
+    public class Cupom : EntidadeBase, IEquatable<Cupom>
+    {
+        public bool Equals(Cupom other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Validar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
