9a5d82d7a926f12a12634f1fc813ae7b5a4d4024
SirLucasWolff <lucaswolff2014@hotmail.com>
2021-09-05 22:19:16 -0300

    Testes do cupom feitos.

--- LocadoraVeiculos.Tests/CupomModule/Cupom.cs
+++ LocadoraVeiculos.Tests/CupomModule/Cupom.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FluentAssertions;
+using LocadoraVeiculos.Dominio.CupomModule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -6,12 +8,57 @@ using System.Text;
 namespace LocadoraVeiculos.Tests.CupomModule
 {
     [TestClass]
-    public class Cupomd
+    [TestCategory("Dominio")]
+    public class CupomTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void DeveValidarTodosOsCampos()
         {
-           
+            var novoCupom = new Cupom("GHTRS", "Desconto", 5000.50m, 5000.50m, "Lucas", DateTime.Now, 1);
+
+            var resultadoValidacao = novoCupom.Validar();
+
+            resultadoValidacao.Should().Be("ESTA_VALIDO");
+        }
+
+        [TestMethod]
+        public void DeveValidarCodigo()
+        {
+            var novoCupom = new Cupom("", "Desconto", 5000.50m, 5000.50m, "Lucas", DateTime.Now, 1);
+
+            var resultadoValidacao = novoCupom.Validar();
+
+            resultadoValidacao.Should().Be("                                                     O campo código não pode estar vazio");
+        }
+
+        [TestMethod]
+        public void DeveValidarTipo()
+        {
+            var novoCupom = new Cupom("GHTRS", "", 5000.50m, 5000.50m, "Lucas", DateTime.Now, 1);
+
+            var resultadoValidacao = novoCupom.Validar();
+
+            resultadoValidacao.Should().Be("                                                     O campo tipo não pode estar vazio");
+        }
+
+        [TestMethod]
+        public void DeveValidarValor()
+        {
+            var novoCupom = new Cupom("GHTRS", "Desconto", 0, 5000.50m, "Lucas", DateTime.Now, 1);
+
+            var resultadoValidacao = novoCupom.Validar();
+
+            resultadoValidacao.Should().Be("                                                     O campo valor não pode ser 0");
+        }
+
+        [TestMethod]
+        public void DeveValidarValorMinimo()
+        {
+            var novoCupom = new Cupom("GHTRS", "Desconto", 5000.50m, 0, "Lucas", DateTime.Now, 1);
+
+            var resultadoValidacao = novoCupom.Validar();
+
+            resultadoValidacao.Should().Be("                                                     O campo valor mínimo não pode ser 0");
         }
     }
 }
--- LocadoraVeiculos.Tests/CupomModule/CupomControlerTest.cs
+++ LocadoraVeiculos.Tests/CupomModule/CupomControlerTest.cs
@@ -32,5 +32,75 @@ namespace LocadoraVeiculos.Tests.CupomModule
             var CupomEncontrado = controlador.SelecionarPorId(novoCupom.Id);
             CupomEncontrado.Codigo.Should().Be(novoCupom.Codigo);
         }
+
+        [TestMethod]
+        public void DeveExlcuir()
+        {
+            //arrange
+            var novoCupom = new Cupom("GHTRS", "Desconto", 5000.50m, 5000.50m, "Lucas", DateTime.Now, 1);
+
+            //action
+            controlador.InserirNovo(novoCupom);
+            controlador.Excluir(novoCupom.Id);
+
+            //assert
+            Cupom CupomExcluido = controlador.SelecionarPorId(novoCupom.Id);
+            CupomExcluido.Should().BeNull();
+        }
+
+        [TestMethod]
+        public void DeveEditar()
+        {
+            //arrange
+            var Cupom = new Cupom("GHTRS", "Desconto", 5000.50m, 5000.50m, "Lucas", DateTime.Now, 1);
+
+            //action
+            controlador.InserirNovo(Cupom);
+
+            var novoCupom = new Cupom("GHTRS", "Desconto", 5000.50m, 5000.50m, "Lucas", DateTime.Now, 1);
+
+            controlador.Editar(Cupom.Id, novoCupom);
+
+            //assert
+            Cupom CupomSelecionado = controlador.SelecionarPorId(Cupom.Id);
+            CupomSelecionado.Codigo.Should().Be(novoCupom.Codigo);
+        }
+
+        [TestMethod]
+        public void DeveSelecionarTodosPorId()
+        {
+            //arrange
+            var Cupom = new Cupom("GHTRS", "Desconto", 5000.50m, 5000.50m, "Lucas", DateTime.Now, 1);
+
+            //action
+            controlador.InserirNovo(Cupom);
+
+            //assert
+            Cupom CupomSelecionado = controlador.SelecionarPorId(Cupom.Id);
+
+            CupomSelecionado.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void DeveSelecionarTodosCuponsOrdenadosPorNome()
+        {
+            //arrange
+            var C1 = new Cupom("GHTRS", "Desconto", 5000.50m, 5000.50m, "Lucas", DateTime.Now, 1);
+            controlador.InserirNovo(C1);
+
+            var C2 = new Cupom("GHJSS", "Desconto", 5000.50m, 5000.50m, "Lucas", DateTime.Now, 1);
+            controlador.InserirNovo(C2);
+
+            var C3 = new Cupom("MJNHDS", "Desconto", 5000.50m, 5000.50m, "Lucas", DateTime.Now, 1);
+            controlador.InserirNovo(C3);
+
+            //action
+            var cupoms = controlador.SelecionarTodos();
+
+            cupoms.Should().HaveCount(3);
+            cupoms[0].Codigo.Should().Be("GHTRS");
+            cupoms[1].Codigo.Should().Be("GHJSS");
+            cupoms[2].Codigo.Should().Be("MJNHDS");
+        }
     }
 }
